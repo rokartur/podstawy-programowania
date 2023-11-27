15c723ef40c7abd88473311aab3d6d8a9eeb4dfe
Artur Rok <rokartur520@gmail.com>
2023-11-27 21:55:16 +0100

    upgrade 2.5

--- Zadanie 2.5 - Pierwiastki równania kwadratowego/Program.cs
+++ Zadanie 2.5 - Pierwiastki równania kwadratowego/Program.cs
@@ -1,6 +1,6 @@
 ﻿class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         Console.WriteLine("Enter a:");
         double a = Convert.ToInt32(Console.ReadLine());
@@ -8,6 +8,25 @@
         double b = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Enter c:");
         double c = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine($"The delta root is: {Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)}");
+        double delta = Math.Pow(b, 2) - 4 * a * c;
+        double deltaRoot = Math.Sqrt(delta);
+        Console.WriteLine("--------------------");
+        Console.WriteLine($"Δ = {delta}");
+
+        if (delta > 0)
+        {
+            Console.WriteLine($"√Δ = {deltaRoot}");
+            double x1 = (-b - deltaRoot) / (2 * a);
+            double x2 = (-b + deltaRoot) / (2 * a);
+            Console.WriteLine($"x1 = {x1}");
+            Console.WriteLine($"x2 = {x2}");
+        } else if (deltaRoot == 0)
+        {
+            double x = -b / (2 * a);
+            Console.WriteLine($"x = {x}");
+        } else
+        {
+            Console.WriteLine("No solution");
+        }
     }
 }
