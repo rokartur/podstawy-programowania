0785d8925f7cea63d0caf92c81f346cb66b017c1
Artur Rok <rokartur520@gmail.com>
2023-11-27 17:03:00 +0100

    Create new project "Zadanie 2.2 - Kalkulator BMI"

---
+++ Zadanie 2.2 - Kalkulator BMI/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your weight in kilograms:");
+        double weight = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter your height in centimeters:");
+        double height = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Your BMI is: " + (weight / ((height / 100) * (height / 100))));
+    }
+}
