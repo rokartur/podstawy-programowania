bb58dec329db5fd4fb8b162ca01bfd08d091f5b0
Artur Rok <rokartur520@gmail.com>
2023-11-27 17:04:08 +0100

    Create new project "Zadanie 2.5 - Pierwiastki równania kwadratowego"

---
+++ Zadanie 2.5 - Pierwiastki równania kwadratowego/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a:");
+        double a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter b:");
+        double b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter c:");
+        double c = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"The delta root is: {Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)}");
+    }
+}
