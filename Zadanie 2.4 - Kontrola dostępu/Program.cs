b2850325ed8bba0dcb1b8e979e1465708377a181
Artur Rok <rokartur520@gmail.com>
2023-11-27 17:03:45 +0100

    Create new project "Zadanie 2.4 - Kontrola dostępu"

---
+++ Zadanie 2.4 - Kontrola dostępu/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("Access granted");
+        }
+        else if (age >= 15)
+        {
+            Console.WriteLine("Do you have parental consent? (y/n)");
+            string answer = Console.ReadLine();
+
+            if (answer.ToLower() == "y")
+            {
+                Console.WriteLine("Access granted");
+            }
+            else if (answer.ToLower() == "n")
+            {
+                Console.WriteLine("Access denied");
+            } else
+            {
+                Console.WriteLine("Wrong answer");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Access denied");
+        }
+    }
+}
