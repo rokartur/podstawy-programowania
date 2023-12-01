6a63bb396231c52f916e63a6b801d7e0553aeddb
Artur Rok <rokartur520@gmail.com>
2023-12-01 21:27:09 +0100

    Create new project "Zadanie 3.3 - Pętla do...while"
    Took 19 minutes

---
+++ Zadanie 3.3 - Pętla do while/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        var i = 1;
+        do
+        {
+            var square = i * i;
+            Console.WriteLine($"The square of {i} is {square}");
+            i++;
+        } while (i <= 20);
+    }
+}
