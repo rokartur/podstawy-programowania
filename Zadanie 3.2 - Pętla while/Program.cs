92ed206382bce48e32e77d7594dc242dc153ca19
Artur Rok <rokartur520@gmail.com>
2023-12-01 21:08:04 +0100

    Create new project "Zadanie 3.2 - Pętla while"
    Took 8 minutes

---
+++ Zadanie 3.2 - Pętla while/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        var sum = 0;
+        var min = int.MaxValue;
+
+        while (true)
+        {
+            Console.Write("Enter a number (0-10): ");
+            var num = Convert.ToInt32(Console.ReadLine());
+
+            if (num is < 0 or > 10)
+            {
+                continue;
+            }
+
+            if (num == 5)
+            {
+                break;
+            }
+
+            if (num < min)
+            {
+                min = num;
+            }
+
+            sum += num * num;
+        }
+
+        Console.WriteLine($"Sum of squares: {sum}");
+        Console.WriteLine($"Minimum number: {min}");
+    }
+}
