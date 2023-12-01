2a062661c26395052c3aa5091e9571494f131209
Artur Rok <rokartur520@gmail.com>
2023-12-01 20:59:54 +0100

    Create new project "Zadanie 3.1 - Pętla for"
    Took 6 minutes

---
+++ Zadanie 3.1 - Pętla for/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        for (var x = 0; x <= 10; x++)
+        {
+            var y = Math.Pow(3, 2) * x;
+            Console.WriteLine($"x = {x}, y = {y}");
+        }
+    }
+}
