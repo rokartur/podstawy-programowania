fd9d7e5dc0077e3ee515a09faf348f8b318ff39c
Artur Rok <rokartur520@gmail.com>
2023-12-01 21:28:33 +0100

    Create new project "Zadanie 3.4 - Pętla foreach"
    Took 1 minute

---
+++ Zadanie 3.4 - Pętla foreach/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        string[] pets = { "cat", "dog", "parrot", "guinea pig" };
+
+        foreach (var pet in pets)
+        {
+            Console.WriteLine(pet);
+            if (pet != "dog") continue;
+            Console.WriteLine("End");
+            Console.WriteLine("Do you want to see full list? (y/n)");
+            var answer = Console.ReadLine();
+
+            if (answer?.ToLower() == "y")
+            {
+                foreach (var pet2 in pets)
+                {
+                    Console.WriteLine(pet2);
+                }
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Bye!");
+                break;
+            }
+        }
+    }
+}
