e8be08a3f742a46809db33f39dd48dd1bee23b57
rokartur <rokartur520@gmail.com>
2024-01-15 05:06:56 +0100

    Create new project "Zadanie 4.2 - Tablica wielowymiarowa"
    Took 2 minutes

---
+++ Zadanie 4.2 - Tablica wielowymiarowa/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        var matrix = new int[10, 10];
+        var sum = 0;
+
+        for (var i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (i == j)
+                {
+                    matrix[i, j] = i;
+                    sum += i;
+                }
+                else
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+        }
+
+        for (var i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Sum of diagonal elements: " + sum);
+    }
+}
