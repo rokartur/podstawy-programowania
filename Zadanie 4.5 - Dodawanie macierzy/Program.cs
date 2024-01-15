5510e897c3a75ac982570d9b61d32484a3091f9e
rokartur <rokartur520@gmail.com>
2024-01-15 05:21:35 +0100

    Create new project "Zadanie 4.5 - Dodawanie macierzy"
    Took 4 minutes

---
+++ Zadanie 4.5 - Dodawanie macierzy/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        const int matrixSize = 10;
+        var matrix1 = new int[matrixSize, matrixSize];
+        var matrix2 = new int[matrixSize, matrixSize];
+        var matrix3 = new int[matrixSize, matrixSize];
+
+        for (var i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix1.GetLength(1); j++)
+            {
+                matrix1[i, j] = 1;
+                matrix2[i, j] = 2;
+            }
+        }
+
+        for (var i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix1.GetLength(1); j++)
+            {
+                matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+
+        Console.WriteLine("Result matrix:");
+        for (var i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix1.GetLength(1); j++)
+            {
+                Console.Write($"{matrix3[i, j]} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
