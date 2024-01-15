fdeff31e723e6b34acbe99f22d34c334a1e2207b
rokartur <rokartur520@gmail.com>
2024-01-15 05:17:14 +0100

    Create new project "Zadanie 4.4 - ArrayList"
    Took 6 minutes

---
+++ Zadanie 4.4 - ArrayList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+class Program
+{
+    private static void Main()
+    {
+        var cities = new ArrayList() { "Gdańsk", "Warszawa", "Katowice", "Poznań", "Kraków", "Rzeszów" };
+        cities.Sort();
+
+        foreach (var city in cities)
+        {
+            Console.WriteLine(city);
+        }
+    }
+}
