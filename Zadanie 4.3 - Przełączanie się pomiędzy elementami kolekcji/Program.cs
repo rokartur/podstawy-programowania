f40869bd05f621d3e9ff6606664c4c3c3822f7c5
rokartur <rokartur520@gmail.com>
2024-01-15 05:11:07 +0100

    Create new project "Zadanie 4.3 - Przełączanie się pomiędzy elementami kolekcji"
    Took 4 minutes

---
+++ Zadanie 4.3 - Przełączanie się pomiędzy elementami kolekcji/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        var table = new int[100];
+        var sumOfEven = 0;
+        var sumOfOdd = 0;
+
+        for (var i = 0; i < table.Length; i++)
+        {
+            table[i] = i + 1;
+        }
+
+        foreach (var number in table)
+        {
+            if (number % 2 == 0)
+            {
+                sumOfEven += number;
+            }
+            else
+            {
+                sumOfOdd += number;
+            }
+        }
+
+        Console.WriteLine($"Summary of even numbers: {sumOfEven}");
+        Console.WriteLine($"Summary of odd numbers: {sumOfOdd}");
+    }
+}
