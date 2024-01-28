f926364a803d02a69ff0e563772868fa4aad43d9
rokartur <rokartur04@icloud.com>
2024-01-28 18:48:28 +0100

    Create new project "Zadanie 5.3 - Operacje na plikach"
    Took 5 minutes

---
+++ Zadanie 5.3 - Operacje na plikach/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        Console.Write("Podaj tytuł książki: ");
+        var bookTitle = Console.ReadLine();
+
+        Console.Write("Podaj nazwisko autora: ");
+        var authorSurname = Console.ReadLine();
+
+        const string filePath = "./library.txt";
+
+        File.WriteAllText(filePath, $"{bookTitle}\n{authorSurname}");
+
+        var fileContent = File.ReadAllLines(filePath);
+
+        Console.WriteLine($"Tytuł książki: {fileContent[0]}");
+        Console.WriteLine($"Nazwisko autora: {fileContent[1]}");
+    }
+}
