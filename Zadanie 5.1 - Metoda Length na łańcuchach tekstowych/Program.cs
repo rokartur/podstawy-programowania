9e6e4c66a6cfc0b0f3278b2e45df3d06a0102988
rokartur <rokartur04@icloud.com>
2024-01-28 18:41:56 +0100

    Create new project "Zadanie 5.1 - Metoda Length na łańcuchach tekstowych"
    Took 6 minutes

---
+++ Zadanie 5.1 - Metoda Length na łańcuchach tekstowych/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        Console.Write("Podaj nazwę miasta: ");
+        var city = Console.ReadLine();
+
+        Console.Write("Podaj nazwę województwa: ");
+        var region = Console.ReadLine();
+
+        var cityLength = city?.Length;
+        var regionLength = region?.Length;
+
+        Console.WriteLine($"Nazwa miasta {city} zawiera {cityLength} liter. Nazwa województwa {region} zawiera {regionLength} liter.");
+    }
+}
