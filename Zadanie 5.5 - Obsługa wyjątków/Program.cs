4b69f16d4241c3cdf8e4bb0cd41117d22e207c52
rokartur <rokartur04@icloud.com>
2024-01-28 18:58:11 +0100

    Create new project "Zadanie 5.5 - Obsługa wyjątków"
    Took 5 minutes

---
+++ Zadanie 5.5 - Obsługa wyjątków/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        var array = new int[10, 10];
+        for (var i = 0; i < 10; i++)
+        {
+            for (var j = 0; j < 10; j++)
+            {
+                array[i, j] = 1;
+            }
+        }
+
+        const string filePath = "dane.txt";
+        // const string anotherFilePathForTest = "test.txt";
+        using (var sw = new StreamWriter(filePath))
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                for (var j = 0; j < 10; j++)
+                {
+                    sw.Write(array[i, j] + " ");
+                }
+                sw.WriteLine();
+            }
+        }
+
+        try
+        {
+            var fileContent = File.ReadAllLines(filePath);
+            foreach (var line in fileContent)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Plik dane.txt nie istnieje.");
+        }
+    }
+}
