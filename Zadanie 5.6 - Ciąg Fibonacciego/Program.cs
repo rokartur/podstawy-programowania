d2a650616dce297bf0bea4ce940dc2e4afce07a6
rokartur <rokartur04@icloud.com>
2024-01-28 19:17:46 +0100

    Create new project "Zadanie 5.6 - Ciąg Fibonacciego"
    Took 18 minutes

---
+++ Zadanie 5.6 - Ciąg Fibonacciego/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    private static int Fibonacci(int n)
+    {
+        switch (n)
+        {
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+            default:
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+
+    private static void Main()
+    {
+        // Console.WriteLine("Podaj numer wyrazu ciągu Fibonacciego:");
+        // var i = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine($"{Fibonacci(i - 1)}, {Fibonacci(i)}, {Fibonacci(i + 1)}");
+
+        for (var i = 0; i < 10; i++)
+        {
+            Console.WriteLine(Fibonacci(i));
+        }
+    }
+}
